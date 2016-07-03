d2601fee28c07523f389859f97a061db155d5f97
Louiefigz <figueroa.aaron@gmail.com>
2016-07-03 15:11:29 -0400

    added Appropriate columns for omniauth callbacks.  Tested and working in the browser.

---
+++ db/migrate/20160703185504_add_column_to_users.rb
@@ -0,0 +1,8 @@
+class AddColumnToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :provider, :string
+    add_column :users, :access_token, :string
+    add_column :users, :uid, :string
+    add_column :users, :twitter_secret, :string
+  end
+end
